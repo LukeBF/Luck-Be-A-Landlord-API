9c81689746fa275949727c46f94441916b160775
Luke Blache-Fraser <resolvetechnologiestt@gmail.com>
2021-05-10 19:30:12 -0400

    added routes to GET a single item and create a new item. No MVC implemented

--- controllers/GameItemsController.js
+++ controllers/GameItemsController.js
@@ -1,20 +1,27 @@
 // Import Express
-const express = require("express");
-// Call the .Router() method on the "express" variable
-const router = express.Router(); 
-//Import models
-const itemModel = require("../model/GameItemModel.js")
+// const express = require("express");
+// const gameItemModel = require("../model/GameItemModel.js");
+// // Call the .Router() method on the "express" variable
+// const router = express.Router(); 
+// //Import models
+// const itemModel = require("../model/GameItemModel.js")
 
-//ROUTES AND ENDPOINTS FOR THE API
-router.get("/",(req,res)=>{
-    res.json({
-        message:"A list of all the game items",
-        data: itemModel.viewAllItems(),
-        total: itemModel.viewAllItems().length
-    })
-});
+// //ROUTES AND ENDPOINTS FOR THE API
+// router.get("/",(req,res)=>{
+//     res.json({
+//         message:"A list of items in the game",
+//         data: itemModel.viewAllItems(),
+//         total: itemModel.viewAllItems().length
+//     })
+// });
+// router.get("/:rarity",(req,res)=>{
+//     const rarityType = req.params.rarity;
+    
+
+
+// })
 
 
 
 // Export the "router" variable 
-module.exports=router;
+//module.exports=router;
--- server.js
+++ server.js
@@ -1,18 +1,105 @@
+//FAKE DATABASE OF ITEMS
+const itemsDB=
+[
+    {
+        name:"Shiny Pebble",
+        rarity:"common",
+        description:"You are 1.1x more likely to find Uncommon, Rare, and Very Rare symbols.",
+        goldPerTurn:0
+    },
+    {
+        name:"Magpie",
+        rarity:"common",
+        description:"Gives 9 gold every 4 spins",
+        goldPerTurn:-1
+    },
+    {
+        name:"Bowling Ball",
+        rarity:"rare",
+        description:"Gives 3 gold for every spin",
+        goldPerTurn:3
+    },
+    {
+        name:"Black Pepper",
+        rarity:"common",
+        description:"Gives 1 gold whenever a symbol is destroyed",
+        goldPerTurn:1
+    },
+    {
+        name:"Goldilocks",
+        rarity:"uncommon",
+        description:"Destroyed and gives 50 gold if you have at least 3 bears",
+        goldPerTurn:50
+    },
+    {
+        name:"Horseshoe",
+        rarity:"uncommon",
+        description:"Gives 2 gold for every spin",
+        goldPerTurn:2
+    },
+    {
+        name:"Four Leav Clover",
+        rarity:"very rare",
+        description:"Gives 4 gold for every spin",
+        goldPerTurn:4
+    }
+]
+
 // Import Express and assign to a variable
 const express = require("express");
-//Import controllers
-const gameItemsController = require("./controllers/GameItemsController.js");
 
 // Create an express app object
 const app = express();
 
-//Create instruction to tell Express how to parse JSON data from the client
+//Create instruction to tell Express how to parse incoming JSON data from the client
 app.use(express.json());
 
 /**
  * Create an app.use for each of the controllers imported
  */
-app.use("/items",gameItemsController)
+//app.use("/items",gameItemsController)
+
+//ROUTE 1-View a list of game items
+app.get("/items",(req,res)=>{
+    res.json({
+        message: "A list of items in the game",
+        data: itemsDB,
+        total: itemsDB.length
+    })
+})
+
+//ROUTE 2-View an item by rarity
+app.get("/items/:item_name",(req,res)=>{
+    
+    const itemName = req.params.item_name;
+    
+    const itemFound = itemsDB.find(item=>item.name===itemName)
+ 
+    if(itemFound != undefined)
+    {
+        res.json({
+            message: `These items were found with the name: ${itemFound.item_name}`,
+            data: itemFound
+        })
+    }
+    else
+    {
+        res.status(404).json({
+            message: "The requested item could not be found"
+        })
+    }
+})
+
+//ROUTE 3-Add a new item
+app.post("/items",(req,res)=>{
+    itemsDB.push(req.body)
+
+    res.json({
+        message: "The item was added successfully",
+        data: req.body
+    })
+})
+
 
 /**
  * Create the web server that listens on a specific port from the "app" variable
