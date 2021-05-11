984e3d1799dce801ae7b659c51519f9e71c12786
Luke Blache-Fraser <resolvetechnologiestt@gmail.com>
2021-05-11 13:42:27 -0400

    added MVC itemsController and itemModel

--- controllers/GameItemsController.js
+++ controllers/GameItemsController.js
@@ -1,27 +1,58 @@
 // Import Express
-// const express = require("express");
+const express = require('express')
+//Create the router object
+const router = express.Router()
+//Import models
+const gameItemModel = require("../model/GameItemModel.js")
+
 // const gameItemModel = require("../model/GameItemModel.js");
 // // Call the .Router() method on the "express" variable
 // const router = express.Router(); 
 // //Import models
 // const itemModel = require("../model/GameItemModel.js")
 
-// //ROUTES AND ENDPOINTS FOR THE API
-// router.get("/",(req,res)=>{
-//     res.json({
-//         message:"A list of items in the game",
-//         data: itemModel.viewAllItems(),
-//         total: itemModel.viewAllItems().length
-//     })
-// });
-// router.get("/:rarity",(req,res)=>{
-//     const rarityType = req.params.rarity;
+//ROUTE 1-View a list of game items
+router.get("/",(req,res)=>{
+    res.json({
+        message: "A list of items in the game",
+        data: gameItemModel.viewAllItems(),
+        total: gameItemModel.viewAllItems().length
+    })
+})
+
+//ROUTE 2-View an item by rarity
+router.get("/:item_name",(req,res)=>{
+    
+    const itemName = req.params.item_name;
+    
+    const itemFound = gameItemModel.viewSingleItem(itemName)
+ 
+    if(itemFound != undefined)
+    {
+        res.json({
+            message: `These items were found with the name: ${itemName}`,
+            data: itemFound
+        })
+    }
+    else
+    {
+        res.status(404).json({
+            message: "The requested item could not be found"
+        })
+    }
+})
+
+//ROUTE 3-Add a new item
+router.post("/",(req,res)=>{
+    
+    gameItemModel.createItem(req.body)
+
+    res.json({
+        message: "The item was added successfully",
+        data: req.body
+    })
+})
     
 
-
-// })
-
-
-
 // Export the "router" variable 
-//module.exports=router;
+module.exports=router;
--- model/GameItemModel.js
+++ model/GameItemModel.js
@@ -1,68 +1,75 @@
-// Create a model to hold the fake database
-// const gameItemModel =
-// {
-//     itemsDB:
-//     [
-//         {
-//             id:1,
-//             name:"Shiny Pebble",
-//             rarity:"common",
-//             description:"You are 1.1x more likely to find Uncommon, Rare, and Very Rare symbols.",
-//             goldPerTurn:0
-//         },
-//         {
-//             id:2,
-//             name:"Magpie",
-//             rarity:"common",
-//             description:"Gives 9 gold every 4 spins",
-//             goldPerTurn:-1
-//         },
-//         {
-//             id:3,
-//             name:"Bowling Ball",
-//             rarity:"rare",
-//             description:"Gives 3 gold for every spin",
-//             goldPerTurn:3
-//         },
-//         {
-//             id:4,
-//             name:"Black Pepper",
-//             rarity:"common",
-//             description:"Gives 1 gold whenever a symbol is destroyed",
-//             goldPerTurn:1
-//         },
-//         {
-//             id:5,
-//             name:"Goldilocks",
-//             rarity:"uncommon",
-//             description:"Destroyed and gives 50 gold if you have at least 3 bears",
-//             goldPerTurn:50
-//         },
-//         {
-//             id:6,
-//             name:"Horseshoe",
-//             rarity:"uncommon",
-//             description:"Gives 2 gold for every spin",
-//             goldPerTurn:2
-//         },
-//         {
-//             id:7,
-//             name:"Four Leav Clover",
-//             rarity:"very rare",
-//             description:"Gives 4 gold for every spin",
-//             goldPerTurn:4
-//         }
-//     ],
+//FAKE DATABASE OF ITEMS
+// Create an object to hold the fake database
+const gameItemModel =
+{
+    itemsDB:
+    [
+        {
+            id:1,
+            name:"Shiny Pebble",
+            rarity:"common",
+            description:"You are 1.1x more likely to find Uncommon, Rare, and Very Rare symbols.",
+            goldPerTurn:0
+        },
+        {
+            id:2,
+            name:"Magpie",
+            rarity:"common",
+            description:"Gives 9 gold every 4 spins",
+            goldPerTurn:-1
+        },
+        {
+            id:3,
+            name:"Bowling Ball",
+            rarity:"rare",
+            description:"Gives 3 gold for every spin",
+            goldPerTurn:3
+        },
+        {
+            id:4,
+            name:"Black Pepper",
+            rarity:"common",
+            description:"Gives 1 gold whenever a symbol is destroyed",
+            goldPerTurn:1
+        },
+        {
+            id:5,
+            name:"Goldilocks",
+            rarity:"uncommon",
+            description:"Destroyed and gives 50 gold if you have at least 3 bears",
+            goldPerTurn:50
+        },
+        {
+            id:6,
+            name:"Horseshoe",
+            rarity:"uncommon",
+            description:"Gives 2 gold for every spin",
+            goldPerTurn:2
+        },
+        {
+            id:7,
+            name:"Four Leav Clover",
+            rarity:"very rare",
+            description:"Gives 4 gold for every spin",
+            goldPerTurn:4
+        }
+    ],
 
-//     viewAllItems()
-//     {
-//         return this.itemsDB;
-//     },
+    viewAllItems()
+    {
+        return this.itemsDB;
+    },
 
-//     viewSingleItem()
-//     {
+    viewSingleItem(itemName)
+    {
+        return this.itemsDB.find(item=>item.name===itemName)
+    },
 
-//     }
-// }
+    createItem(item)
+    {
+        this.itemsDB.push(item)
+    }
 
-//module.exports=gameItemModel;
+}
+
+module.exports=gameItemModel;
